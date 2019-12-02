a3ba0cdf8c2ebe7d66715a88c768907eeeb47568
陈鹏 <simple19930611@gmail.com>
2019-12-02 16:34:51 +0800

    update:migrate androidx

--- app/src/main/java/com/zhy/view/ArticleActivity.java
+++ app/src/main/java/com/zhy/view/ArticleActivity.java
@@ -1,9 +1,10 @@
 package com.zhy.view;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ArticleActivity extends AppCompatActivity
 {
